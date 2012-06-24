557e6b73dc56fea4b2744f11a9c5e4d0afeb4a5a
Xinghong Fang <xinghong.fang@gmail.com>
2012-06-24 17:51:23 +0100

    build-ba.sh updated

--- deployment/build-ba.sh
+++ deployment/build-ba.sh
@@ -17,13 +17,17 @@ cd -
 
 badebug "Compiling blackadder..."
 cd blackadder/src
+autoconf
 ./ndk-make-ba.sh
 make install
 cd -
 
+badebug "Copying libgnustl_shared.so..."
+cp $ANDROID_NDK/sources/cxx-stl/gnu-libstdc++/libs/armeabi-v7a/libgnustl_shared.so $CLICK_PREFIX/lib
+
 badebug "Removing unnecessary files..."
 rm -r $CLICK_PREFIX/share
 rm -r $CLICK_PREFIX/include
-find $CLICK_PREFIX/bin | grep -v click$ | xargs rm
+find $CLICK_PREFIX/bin -type f | grep -v click$ | xargs rm
 
 
